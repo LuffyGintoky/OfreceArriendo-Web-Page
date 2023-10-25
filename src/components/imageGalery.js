a7978f72aba324255e9ce45226aa8ad7946b1d8a
David <david.pro.rojas@gmail.com>
2023-10-25 13:30:01 -0300

    as

--- src/components/imageGalery.js
+++ src/components/imageGalery.js
@@ -21,7 +21,7 @@ class ImageGallery extends HTMLElement {
             </div>
 
             <div class="flex-container" >
-                <button type="button" class="btn btn-outline-danger">Volver Al Catalogo
+                <button  id="returnButton" type="button" class="btn btn-outline-danger"> Volver Al Catalogo
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-90deg-left" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M1.146 4.854a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H12.5A2.5 2.5 0 0 1 15 6.5v8a.5.5 0 0 1-1 0v-8A1.5 1.5 0 0 0 12.5 5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4z"/>
                     </svg>
@@ -35,6 +35,8 @@ class ImageGallery extends HTMLElement {
         this.shadowRoot.querySelector('#prevButton').addEventListener('click', () => this.changeImg(-1));
         this.shadowRoot.querySelector('#nextButton').addEventListener('click', () => this.changeImg(1));
         this.shadowRoot.querySelector('#closeButton').addEventListener('click', () => this.closeFullImg());
+        this.shadowRoot.querySelector('#returnButton').addEventListener('click', () => this.returnToCatalog());
+        
         this.currentImgIndex = 0;
         this.imageNamesArray = [];
 
@@ -91,6 +93,7 @@ class ImageGallery extends HTMLElement {
         this.loadImages(imageNamesJSON);
 
         this.imageNamesArray = JSON.parse(imageNames);
+        document.addEventListener('keydown', this.handleKeydown.bind(this));
     }
 
     loadImages(imageArray) {
@@ -146,8 +149,15 @@ class ImageGallery extends HTMLElement {
         fullImg.src = imagesSrc + this.imageNamesArray[this.currentImgIndex];
     }
 
+    returnToCatalog(){
+            window.history.back();
+    }
 
-
+    handleKeydown(evt) {
+        if (evt.keyCode === 27) {  // 27 es el código de la tecla "Esc"
+            this.returnToCatalog();
+        }
+    }
 
 
 }
